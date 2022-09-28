f2850e6cda4283126c877a12bc413e30f26ddabb
shubham123ch <shubham123ch@gmail.com>
2022-09-28 22:13:11 +0530

    initial basic setup

--- template.py
+++ template.py
@@ -5,13 +5,13 @@ logging.basicConfig(level=logging.INFO, format ='[%(asctime)s]: %(message)s: ')
 PACKAGE_NAME = "deepClassifier"
 LIST_OF_FILES = [
     ".github/workflows/.gitkeep",
-    f"src/{PACKAGE_NAME}/__init.py",
-    f"src/{PACKAGE_NAME}/components/__init.py",
-    f"src/{PACKAGE_NAME}/utils/__init.py",
-    f"src/{PACKAGE_NAME}/config/__init.py",
-    f"src/{PACKAGE_NAME}/pipeline/__init.py",
-    f"src/{PACKAGE_NAME}/entity/__init.py",
-    f"src/{PACKAGE_NAME}/constants/__init.py",
+    f"src/{PACKAGE_NAME}/__init__.py",
+    f"src/{PACKAGE_NAME}/components/__init__.py",
+    f"src/{PACKAGE_NAME}/utils/__init__.py",
+    f"src/{PACKAGE_NAME}/config/__init__.py",
+    f"src/{PACKAGE_NAME}/pipeline/__init__.py",
+    f"src/{PACKAGE_NAME}/entity/__init__.py",
+    f"src/{PACKAGE_NAME}/constants/__init__.py",
     "configs/config.yaml",
     "dvc.yaml",
     "params.yaml",
@@ -29,14 +29,14 @@ LIST_OF_FILES = [
 
 for filepath in LIST_OF_FILES:
     filepath = Path(filepath)
-    filedir,filename = os.path.split(filepath)
+    filedir, filename = os.path.split(filepath)
     if filedir != "":
-        os.makedirs(filedir,exist_ok = True)
+        os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creatiing directory:{filedir} for file:{filename}")
         
-        if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-            with open(filepath, "w") as f:
-                pass # creating an empty file
-                logging.info(f"Creating empty file:{filepath}")
-        else:
-            logging.info(f"{filename} already exists")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass # creating an empty file
+            logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} already exists")
