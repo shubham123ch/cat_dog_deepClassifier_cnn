2910c88905db6b56916cf5555dd3ca3102552397
shubham123ch <shubham123ch@gmail.com>
2022-10-01 19:38:38 +0530

    envirnoment "init_setup.sh " creation

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
 echo [$(date)]: "START" 
 echo [$(date)]: "creating env with python 3.8 version" 
-conda create --prefix ./env python=3.8 -y
+conda create --prefix ./mldlcvtemplates python=3.8 -y
 echo [$(date)]: "activating the environment" 
-source activate ./env
+source activate ./mldlcvtemplates
 echo [$(date)]: "installing the dev requirements" 
 pip install -r requirements_dev.txt
 echo [$(date)]: "END" 
